5d0b939b9ba10ed06daa46cee714ca08e926ff3c
unknown <meddah.abdellah.spcx@gmail.com>
2020-01-25 22:42:54 +0100

    localSession

--- www/js/BLE.js
+++ www/js/BLE.js
@@ -1,87 +1,89 @@
-var service_uuid="0000180d-0000-1000-8000-00805f9b34fb";
-var characteristic_uuid="00002A37-0000-1000-8000-00805f9b34fb";
+var service_uuid = "0000180d-0000-1000-8000-00805f9b34fb";
+var characteristic_uuid = "00002A37-0000-1000-8000-00805f9b34fb";
 var lastReadingTime = new Date().getTime();
+
 function startBLE(button) {
-  if(button!=undefined){
+  if (button != undefined) {
     button.text("Scanning ...");
-  /*  button.on("click",function(){
-      if(confirm("Scanning For Device Do You Want To Interrupt The Scan"))ble.stopScan(function(){},function(){});
-    });*/
+    /*  button.on("click",function(){
+        if(confirm("Scanning For Device Do You Want To Interrupt The Scan"))ble.stopScan(function(){},function(){});
+      });*/
   }
   ble.startScan([service_uuid], function(device) {
-        ble.autoConnect(device.id,function(){
-        alert("connected to BLE device");
-        app.bleConnected = true;
-        app.bleDevice_id = device.id;
-        button.text("Disconnect Bluetooth");
-        //app.loadView(1).then(app.initCssStates).then(app.initButtons);
-        ble.startNotification(device.id,service_uuid, characteristic_uuid, function(pData) {
-            pData = new Uint8Array(pData);
-            //SHOW THE DATA HERE
+    ble.autoConnect(device.id, function() {
+      alert("connected to BLE device");
+      app.bleConnected = true;
+      app.bleDevice_id = device.id;
+      button.text("Disconnect Bluetooth");
+      //app.loadView(1).then(app.initCssStates).then(app.initButtons);
+      ble.startNotification(device.id, service_uuid, characteristic_uuid, function(pData) {
+        pData = new Uint8Array(pData);
+        //SHOW THE DATA HERE
 
-              if (pData.length >= 1) var hr = pData[1];
-              var hrFormat = pData[0] & 0x01;
-              var sensorContact = true;
-              var contactSupported = !((pData[0] & 0x06) == 0);
-              if ( contactSupported ) {
-                sensorContact = ((pData[0] & 0x06) >> 1) == 3;
-              }
-              var energyExpended = (pData[0] & 0x08) >> 3;
-              var rrPresent = (pData[0] & 0x10) >> 4;
-              var hrValue = (hrFormat == 1 ? pData[1] + (pData[2] << 8) : pData[1]) & (hrFormat == 1 ? 0x0000FFFF : 0x000000FF);
-              $("#hr_val").text(hrValue+" bpm");
-              if ( !contactSupported && hrValue == 0 ) {
-                // note does this apply to all sensors, also 3rd party
-                sensorContact = false;
-              }
-              var sensorContactFinal = sensorContact;
-              var offset = hrFormat + 2;
-              var energy = 0;
-              if (energyExpended == 1) {
-                energy = (pData[offset] & 0xFF) + ((pData[offset + 1] & 0xFF) << 8);
-                offset += 2;
-              }
-              if (rrPresent == 1) {
-                var len = pData.length;
-                while (offset < len) {
-                  var rrValue = parseInt((pData[offset] & 0xFF) + ((pData[offset + 1] & 0xFF) << 8));
-                  offset += 2;
-                  console.log("leastReading: "+lastReadingTime);
-                  console.log("Now: "+new Date().getTime());
-                  var timeDiff = new Date().getTime()-lastReadingTime;
-                  console.log("timeDiff: "+timeDiff);
-                  //Send data to server
-                  if(app.sendEnabled){
-                    var load = rrValue+","+timeDiff+","+app.session_id+","+hrValue;
-                    console.log("MQTT sending");
-                    console.log(load);
-                    mqttClient.publish("data"+app.device_id, load);
-                    lastReadingTime = new Date().getTime();
-                  }
-                }
-              }
+        if (pData.length >= 1) var hr = pData[1];
+        var hrFormat = pData[0] & 0x01;
+        var sensorContact = true;
+        var contactSupported = !((pData[0] & 0x06) == 0);
+        if (contactSupported) {
+          sensorContact = ((pData[0] & 0x06) >> 1) == 3;
+        }
+        var energyExpended = (pData[0] & 0x08) >> 3;
+        var rrPresent = (pData[0] & 0x10) >> 4;
+        var hrValue = (hrFormat == 1 ? pData[1] + (pData[2] << 8) : pData[1]) & (hrFormat == 1 ? 0x0000FFFF : 0x000000FF);
+        $("#hr_val").text(hrValue + " bpm");
+        if (!contactSupported && hrValue == 0) {
+          // note does this apply to all sensors, also 3rd party
+          sensorContact = false;
+        }
+        var sensorContactFinal = sensorContact;
+        var offset = hrFormat + 2;
+        var energy = 0;
+        if (energyExpended == 1) {
+          energy = (pData[offset] & 0xFF) + ((pData[offset + 1] & 0xFF) << 8);
+          offset += 2;
+        }
+        if (rrPresent == 1) {
+          var len = pData.length;
+          while (offset < len) {
+            var rrValue = parseInt((pData[offset] & 0xFF) + ((pData[offset + 1] & 0xFF) << 8));
+            offset += 2;
+            console.log("leastReading: " + lastReadingTime);
+            console.log("Now: " + new Date().getTime());
+            var timeDiff = new Date().getTime() - lastReadingTime;
+            console.log("timeDiff: " + timeDiff);
+            //Send data to server
+            if (app.sendEnabled) {
+              var load = rrValue + "," + timeDiff + "," + app.session_id + "," + hrValue;
+              console.log("MQTT sending");
+              console.log(load);
+              mqttClient.publish("data" + app.device_id, load);
+              lastReadingTime = new Date().getTime();
+            }
+          }
+        }
 
-        }, function(){});
-      },function(){});
+      }, function() {});
+    }, function() {});
 
-  },function(){ alert("Device Not Found")});
+  }, function() {
+    alert("Device Not Found")
+  });
 }
 
-function writeBLE(data,id){
-  ble.write(id, service_uuid, characteristic_uuid, stringToBytes(data), function(){
-  }, function(){
-  });
+function writeBLE(data, id) {
+  ble.write(id, service_uuid, characteristic_uuid, stringToBytes(data), function() {}, function() {});
 }
 
 /* Drivers */
 function stringToBytes(string) {
-   var array = new Uint8Array(string.length);
-   for (var i = 0, l = string.length; i < l; i++) {
-       array[i] = string.charCodeAt(i);
-    }
-    return array.buffer;
+  var array = new Uint8Array(string.length);
+  for (var i = 0, l = string.length; i < l; i++) {
+    array[i] = string.charCodeAt(i);
+  }
+  return array.buffer;
 }
+
 function bytesToString(buffer) {
-    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  return String.fromCharCode.apply(null, new Uint8Array(buffer));
 }
 /* END Drivers */
--- www/js/mqtt.js
+++ www/js/mqtt.js
@@ -1,60 +1,45 @@
-var MQTTconnected=false;
-var MQTTtopic="";
-if(localStorage.getItem("MQTTtopic")!==null){
-  MQTTtopic=localStorage.getItem("MQTTtopic")
+var MQTTconnected = false;
+var MQTTtopic = "";
+if (localStorage.getItem("MQTTtopic") !== null) {
+  MQTTtopic = localStorage.getItem("MQTTtopic")
 }
-function startMQTT(){
-  /*
-  var client  = mqtt.connect('mqtt://52.30.226.105/')
-  client.on('error', function (error) {
-    alert("error : "+error);
-  });
-  client.on('connect', function () {
-    alert("connected");
-    client.subscribe('presence', function (err) {
-      alert(err);
-      if (!err) {
-        client.publish('presence', 'Hello mqtt')
-      }
-    })
-  });
-*/
-cordova.plugins.CordovaMqTTPlugin.connect({
-    url:"tcp://18.191.166.212", //a public broker used for testing purposes only. Try using a self hosted broker for production.
-    port:80,
-    clientId:"Hani",
-    connectionTimeout:3000,
-    username:"uname",
-    password:'pass',
-    keepAlive:60,
+
+function startMQTT() {
+  cordova.plugins.CordovaMqTTPlugin.connect({
+    url: "tcp://18.191.166.212", //a public broker used for testing purposes only. Try using a self hosted broker for production.
+    port: 80,
+    clientId: "Hani",
+    connectionTimeout: 3000,
+    username: "uname",
+    password: 'pass',
+    keepAlive: 60,
     isBinaryPayload: false, //setting this 'true' will make plugin treat all data as binary and emit ArrayBuffer instead of string on events
-    success:function(s){
-        alert("Connect to Device through Internet");
-        MQTTconnected=true;
+    success: function(s) {
+      alert("Connect to Device through Internet");
+      MQTTconnected = true;
     },
-    error:function(e){
-        alert("Connection to Device through Internet Error");
+    error: function(e) {
+      alert("Connection to Device through Internet Error");
     },
-    onConnectionLost:function (){
-        alert("Disconnected From Internet");
-        MQTTconnected=false;
+    onConnectionLost: function() {
+      alert("Disconnected From Internet");
+      MQTTconnected = false;
     },
 
-});
+  });
 }
-function writeMQTT(data){
-  if(MQTTconnected){
-    if(MQTTtopic.length>1){
+
+function writeMQTT(data) {
+  if (MQTTconnected) {
+    if (MQTTtopic.length > 1) {
       cordova.plugins.CordovaMqTTPlugin.publish({
-         topic:MQTTtopic,
-         payload:data,
-         qos:0,
-         retain:false,
-         success:function(s){
-         },
-         error:function(e){
-         }
+        topic: MQTTtopic,
+        payload: data,
+        qos: 0,
+        retain: false,
+        success: function(s) {},
+        error: function(e) {}
       })
-    }else alert("Not Connected to Device through Internet.")
-  }else alert("Not Connected to Internet");
+    } else alert("Not Connected to Device through Internet.")
+  } else alert("Not Connected to Internet");
 }
